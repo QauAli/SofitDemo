ef5786519a97e95f1c3900efeb4eed0c7a6da8d2
QauAli <muhammadali46000@gmail.com>
2020-08-11 19:55:25 +0500

    view binding added in all fragments

--- app/src/main/java/com/duplicatefile/remover/file/sofitdemo/Fragments/HomeFragment.java
+++ app/src/main/java/com/duplicatefile/remover/file/sofitdemo/Fragments/HomeFragment.java
@@ -1,66 +1,55 @@
 package com.duplicatefile.remover.file.sofitdemo.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.duplicatefile.remover.file.sofitdemo.R;
+import com.duplicatefile.remover.file.sofitdemo.databinding.FragmentHomeBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private FragmentHomeBinding mBinding;
+    private Context mContext;
     public HomeFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        mBinding = FragmentHomeBinding.inflate(inflater, container, false);
+        View view = mBinding.getRoot();
+        mContext = view.getContext();
+        setViews();
+        return view;
+    }
+    private void setViews() {
+        CircularProgressDrawable circularProgressDrawable = new CircularProgressDrawable(mContext);
+        circularProgressDrawable.setStrokeWidth(5f);
+        circularProgressDrawable.setCenterRadius(30f);
+        circularProgressDrawable.start();
+
+        Glide.with(mContext)
+                .applyDefaultRequestOptions(RequestOptions.centerCropTransform()
+                        .diskCacheStrategy(DiskCacheStrategy.RESOURCE))
+                .load("http://goo.gl/gEgYUd")
+                .placeholder(circularProgressDrawable)
+                .into(mBinding.imageView);
     }
 }
